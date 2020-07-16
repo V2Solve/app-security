54cb6e8ea22040ef13b0a679116de31d7aa69694
Saurinya <Saurinya@host.docker.internal>
2020-07-16 00:12:53 -0400

    updated to create a dockerfile for postgres sql

--- docker/initialization.sql
+++ docker/initialization.sql
@@ -398,3 +398,78 @@ NOT DEFERRABLE;
 -- SQL Sequences start here..
 
 
+CREATE SEQUENCE "seq_action_id" INCREMENT 1  START 10000  MINVALUE 1  MAXVALUE 1000000000 CACHE 1;
+ALTER SEQUENCE "seq_action_id" OWNER TO postgres;
+
+CREATE SEQUENCE "seq_application_id" INCREMENT 1  START 10000  MINVALUE 1  MAXVALUE 1000000000 CACHE 1;
+ALTER SEQUENCE "seq_application_id" OWNER TO postgres;
+
+CREATE SEQUENCE "seq_client_id" INCREMENT 1  START 10000  MINVALUE 1  MAXVALUE 1000000000 CACHE 1;
+ALTER SEQUENCE "seq_client_id" OWNER TO postgres;
+
+CREATE SEQUENCE "seq_client_group_id" INCREMENT 1  START 10000  MINVALUE 1  MAXVALUE 1000000000 CACHE 1;
+ALTER SEQUENCE "seq_client_group_id" OWNER TO postgres;
+
+CREATE SEQUENCE "seq_client_group_membership_id" INCREMENT 1  START 10000  MINVALUE 1  MAXVALUE 1000000000 CACHE 1;
+ALTER SEQUENCE "seq_client_group_membership_id" OWNER TO postgres;
+
+CREATE SEQUENCE "seq_client_group_roles_id" INCREMENT 1  START 10000  MINVALUE 1  MAXVALUE 1000000000 CACHE 1;
+ALTER SEQUENCE "seq_client_group_roles_id" OWNER TO postgres;
+
+CREATE SEQUENCE "seq_client_role_id" INCREMENT 1  START 10000  MINVALUE 1  MAXVALUE 1000000000 CACHE 1;
+ALTER SEQUENCE "seq_client_role_id" OWNER TO postgres;
+
+CREATE SEQUENCE "seq_client_role_permissions_id" INCREMENT 1  START 10000  MINVALUE 1  MAXVALUE 1000000000 CACHE 1;
+ALTER SEQUENCE "seq_client_role_permissions_id" OWNER TO postgres;
+
+CREATE SEQUENCE "seq_permission_id" INCREMENT 1  START 10000  MINVALUE 1  MAXVALUE 1000000000 CACHE 1;
+ALTER SEQUENCE "seq_permission_id" OWNER TO postgres;
+
+CREATE SEQUENCE "seq_resource_id" INCREMENT 1  START 10000  MINVALUE 1  MAXVALUE 1000000000 CACHE 1;
+ALTER SEQUENCE "seq_resource_id" OWNER TO postgres;
+
+CREATE SEQUENCE "seq_resource_domain_id" INCREMENT 1  START 10000  MINVALUE 1  MAXVALUE 1000000000 CACHE 1;
+ALTER SEQUENCE "seq_resource_domain_id" OWNER TO postgres;
+
+CREATE SEQUENCE "seq_resource_domain_type_id" INCREMENT 1  START 10000  MINVALUE 1  MAXVALUE 1000000000 CACHE 1;
+ALTER SEQUENCE "seq_resource_domain_type_id" OWNER TO postgres;
+
+CREATE SEQUENCE "seq_role_scope_id" INCREMENT 1  START 10000  MINVALUE 1  MAXVALUE 1000000000 CACHE 1;
+ALTER SEQUENCE "seq_role_scope_id" OWNER TO postgres;
+
+CREATE SEQUENCE "seq_scope_type_id" INCREMENT 1  START 10000  MINVALUE 1  MAXVALUE 1000000000 CACHE 1;
+ALTER SEQUENCE "seq_scope_type_id" OWNER TO postgres;
+
+-- NOW INITIALIZING THE DATABASE with some initial Data.
+
+-- create the first client..
+insert into client (id,client_identifier,description,application_id) values (1,'saurinya@gmail.com','This client is built in super user.',null);
+
+-- create the super group
+insert into client_group (id,name,description,application_id) values (1,'SUPER_CLIENTS','This group is a built in starter super client group',null);
+
+
+-- create the connection between the super user and the super group..
+insert into client_group_membership (id,client_id,client_group_id,application_id) values (1,1,1,null);
+
+-- create the super action
+insert into action (id,name,description,application_id) values (1,'ALL_ACTIONS','The super action that stands for all actions',null);
+
+-- create the super resource
+insert into resource (id,name,description,application_id) values (1,'ALL_RESOURCES','The super resource keyword that stands for all resources',null);
+
+
+-- create the super permission
+insert into permission (id,name,action_id,resource_id,description,application_id) values (1,'SUPER_PERMISSION',1,1,'This permission provides ALL_ACTIONS on ALL_RESOURCES - Its a super permission.',null);
+
+
+-- create the super role..
+insert into client_role (id,name,description,application_id) values (1,'SUPER_ROLE','This is a super role, which should have super permissions.',null);
+
+
+-- give the super role, the super permission
+insert into client_role_permissions (id,client_role_id,permission_id,value,application_id) values (1,1,1,'allow',null);
+
+
+-- assign the super role to the super clients group..
+insert into client_group_roles (id,client_group_id,client_role_id,resource_domain_id,role_scope_id,application_id) values (1,1,1,null,null,null);
